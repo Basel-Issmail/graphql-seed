480952ff78e43fa2c8de77ba8157a0d3d86d1ca3
Basel Issmail <basel.issmail@gmail.com>
2018-08-03 04:07:54 +0300

    add logger util

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import config from './server/config/config';
 import { app, server } from './server/server';
+import logger from './server/utils/logger';
 
-app.listen({ port: config.port }, () => console.log(`🚀 Server ready at http://localhost:${config.port}${server.graphqlPath}`));
+app.listen({ port: config.port }, () => logger.log(`🚀 Server ready at http://localhost:${config.port}${server.graphqlPath}`));
